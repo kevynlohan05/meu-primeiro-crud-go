61b2e710ed4b00e64a336dacf05ff772c2f47155
Kevyn Lohan <kevynlohan05@gmail.com>
2025-05-02 06:01:23 -0300

    Validando campos ao criar usuário

--- src/controller/model/request/user_request.go
+++ src/controller/model/request/user_request.go
@@ -1,9 +1,9 @@
 package request
 
 type UserRequest struct {
-	Name       string `json:"name"`
-	Email      string `json:"email"`
-	Password   string `json:"password"`
-	Department string `json:"department"`
-	Role       string `json:"role"` // "admin" ou "user"
+	Name       string `json:"name" binding:"required,min=3,max=100"`
+	Email      string `json:"email" binding:"required,email"`
+	Password   string `json:"password" binding:"required,min=6,max=20"`
+	Department string `json:"department" binding:"required"`
+	Role       string `json:"role" binding:"required"` // "admin" ou "user"
 }
